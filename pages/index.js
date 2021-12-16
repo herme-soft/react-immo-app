b7c61da1ee2f0751074755bc0656a13ac1887763
enochndika <enochbeloved@gmail.com>
2021-12-16 17:03:08 +0100

    fix all erros - turn off @next/next/no-img-element eslint rule - turn off react/no-unescaped-entities eslint rule - remove sync scripts load - update .eslintrc.json - remove nprogress custom stylesheet load

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,6 @@ import { Features } from "../components/features"
 export default function Home({ propertiesVip, properties }) {
   return (
     <>
-    <Head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    </Head>
-
     <Layout footer>
       <Carousel />
       {/* {JSON.stringify(propertiesVip)} */}
